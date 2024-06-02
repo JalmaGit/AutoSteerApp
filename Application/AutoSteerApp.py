7e5a99ab4ed1d02ecd670394ed07a487e4738ff2
JalmaGit <jmbb00@hotmail.com>
2024-06-02 02:33:25 +0200

    Update

--- Application/AutoSteerApp.py
+++ Application/AutoSteerApp.py
@@ -1,6 +1,10 @@
 from tkinter import *
 import tkintermapview
 
+def left_click_event(coords):
+    print("Add marker:", coords)
+    marker = mapWidget.set_marker(coords[0], coords[1], text="new marker")
+
 root = Tk()
 root.geometry("1920x1080")
 root.title("Auto Steer")
@@ -13,12 +17,17 @@ label.pack(pady=10)
 
 myLabel = LabelFrame(root)
 myLabel.pack(pady=50)
-
-mapWidget = tkintermapview.TkinterMapView(myLabel,width=600,height=400)
+    
+mapWidget = tkintermapview.TkinterMapView(myLabel,width=1000,height=800)
 mapWidget.set_tile_server("https://services.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}")
 mapWidget.set_position(59.22467, 10.33620)
 mapWidget.set_zoom(15)
 
+mapWidget.add_left_click_map_command(left_click_event)
+
+
 mapWidget.pack()
 
 root.mainloop()
+
+print()
